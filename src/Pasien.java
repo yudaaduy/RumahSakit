6efb28feb11ab5968bc03ced28836550abd661f0
Unknown <rizal160692@gmail.com>
2016-12-22 19:39:19 +0700

    Create Main in berobat, create method in pasien

--- src/Pasien.java
+++ src/Pasien.java
@@ -1,4 +1,32 @@
 
 public class Pasien extends Manusia {
-
+	RumahSakit lokasiRumahSakit;
+	RumahPasien lokasiRumahPasien;
+		
+		public void setLokasi(BendaMati lokasi) {
+			this.lokasiRumahpasien = (Rumahpasien) lokasi;
+		}
+		public RumahSakit perjalanan(BendaMati rumahSakit) {
+			RumahSakit berobat = new RumahSakit(this.lokasiRumahpasien, lokasiRumahSakit);
+			return berobat.pasienJalan();
+		}
+		
+		public boolean pasienJalan() {
+			return pasienJalan.valid;
+		}
+		public void pergiJalan(RumahSakit pasienPergiKeRumahSakit) {
+			pasienBerobat = pasienPergiKeRumahSakit;
+		}
+		public boolean pasienPergiJalan() {
+			return pasienBerobat.valid;
+		}
+		public int jalankeRumahSakit() {
+			return pasienBerobat.lokasiAwal.koordinat;
+		}
+		public void pasienSehat() {
+			pasienberobat.valid = false;
+		}
+	
+	
+	
 }
