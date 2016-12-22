ee88423eb706182f8426127ff3b638b4912b4868
Unknown <yudaprsetyo@gmail.com>
2016-12-22 19:37:16 +0700

    admin rs

--- src/Admin.java
+++ src/Admin.java
@@ -1,4 +1,15 @@
-
 public class Admin extends Manusia {
+	Ruang Ruang;
+	Dokter Dokter;
 
+	public void cekRuangan (Ruang Ruang, Dokter Dokter){
+		if(Pasien.Status == 1){
+			System.out.print("Pasien Dirawat");
+		}else{
+			System.out.print("Ruangan dipakai");
+		}
+		}
+		
+	}
 }
+
