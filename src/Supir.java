9e6b6eec8efb4a2f2c475bcabb9a31dea28b3264
Unknown <rizal160692@gmail.com>
2016-12-23 11:19:34 +0700

    add supir.java

--- src/Supir.java
+++ src/Supir.java
@@ -1,4 +1,28 @@
 
 public class Supir extends Manusia {
+	MobilJalan grabKerja;
+	TempatMakan tempatMangkal;
 
+	public void setLokasiMangkal(BendaMati lokasiWarteg) {
+		this.tempatMangkal = (TempatMakan) lokasiWarteg;
+	}
+	public MobilJalan cariMangsa(BendaMati lokasiWarteg) {
+		MobilJalan grabBeroperasi = new MobilJalan(this.tempatMangkal, lokasiWarteg);
+		return grabBeroperasi.perjalananYuda();
+	}
+	public boolean grabKerja() {
+		return grabKerja.valid;
+	}
+	public void dapetPenumpang(MobilJalan jemputYuda) {
+		grabKerja = jemputYuda;
+	}
+	public boolean begalYuda() {
+		return grabKerja.valid;
+	}
+	public int jalan() {
+		return grabKerja.lokasiAwal.koordinat;
+	}
+	public void begalMales() {
+		grabKerja.valid = false;
+	}
 }
