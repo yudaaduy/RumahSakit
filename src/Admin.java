60c4ccde8532b7fb93c9db2f5ac8cc4683461482
Unknown <yudaprsetyo@gmail.com>
2016-12-23 18:38:28 +0700

    sebelum pulang

--- src/Admin.java
+++ src/Admin.java
@@ -1,15 +1,17 @@
 public class Admin extends Manusia {
 	Ruang ruang;
 	Dokter dokter;
-	Duit duit;
 	int biaya;
 
-<<<<<<< HEAD
+public boolean pasienMembayar (Duit){
+	if(!Duit == membayar);
+	
+	
+}
 public int membayar (Ruang ruang, Dokter dokter, int membayar){
 	System.out.println(ruang.hargaRuang + dokter.hargaDokter);
 	return membayar;
 }	
-
 public void cekRuangan (Ruang ruang, Dokter dokter){
 		if(Pasien.status == 1){ 
 			System.out.print("Pasien Dirawat");
@@ -18,17 +20,12 @@ public void cekRuangan (Ruang ruang, Dokter dokter){
 			System.out.print("Ruangan kosong");
 		}
 	}
+public void cekRuangan (Ruang Ruang, Dokter Dokter){
+		if(Pasien.Status == 1){
+			System.out.print("Pasien Dirawat");
+			System.out.print("Ruangan dipakai");
+		}else{
+			System.out.print("Ruangan kosong");
+		}
+	}		
 }
-=======
-	//public void cekRuangan (Ruang Ruang, Dokter Dokter){
-//		if(Pasien.Status == 1){
-//			System.out.print("Pasien Dirawat");
-//			System.out.print("Ruangan dipakai");
-//		}else{
-//			System.out.print("Ruangan kosong");
-//		}
-//		}
-		
-	}
-
->>>>>>> master
