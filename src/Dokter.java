e0cb1a4d9b3f45c632aa7701f4e2fe84f3360940
Unknown <yudaprsetyo@gmail.com>
2016-12-26 09:09:44 +0700

    senin pagi dokter sama obat

--- src/Dokter.java
+++ src/Dokter.java
@@ -1,5 +1,5 @@
 public class Dokter extends Manusia {
-<<<<<<< HEAD
+
 	String nama;
 	String spesialis;
 	Pasien pasien;
@@ -15,21 +15,13 @@ public class Dokter extends Manusia {
 			System.out.print("Pasien harus rawat inap");
 		}
 	}
-}
-=======
-	String Nama;
-	String Spesialis;
-	Pasien Pasien;
-	Penyakit penyakit;
-	int Status;
-	String Resep;
 
-//public void periksaPasien(Pasien Pasien, Penyakit Penyakit, String Resep){
-//	if(Pasien.penyakit == Penyakit){
-//		System.out.print("Resep obat" + Penyakit);
-//	}else{
-//		System.out.print("Surat rujukan ke rumah sakit pusat");
-//	}
-//	}
+public void periksaPasien(Pasien Pasien, Penyakit Penyakit, String Resep){
+	if(Pasien.penyakit == Penyakit){
+		System.out.print("Resep obat" + Penyakit);
+	}else{
+		System.out.print("Surat rujukan ke rumah sakit pusat");
+	}
+	}
 }
->>>>>>> master
+
--- src/Obat.java
+++ src/Obat.java
@@ -2,8 +2,4 @@ public class Obat extends BendaMati {
 	String namaObat;
 	int jumlah;
 	int harga;
-
-	public void Statobat(String NamaObat, int Jumlah, int Harga){
-		
-	}
 }
