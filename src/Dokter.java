1e783b97b86d9b2ad508a8e37c10477396bc34de
Unknown <yudaprsetyo@gmail.com>
2016-12-27 14:09:10 +0700

    al

--- src/Dokter.java
+++ src/Dokter.java
@@ -1,61 +1,35 @@
-public class Dokter extends Manusia {
-
-
-	String nama;
-	String spesialis;
-	Pasien pasien;
-	int status;
-	String resep;
-	int hargaDokter;
-
-//	public void periksaPasienRawat(Pasien Pasien, Penyakit Penyakit, String Resep){
-//		if(Pasien.penyakit){
-//			System.out.print("Resep obat" + Penyakit);
-//		}else{
-//			System.out.print("Pasien harus rawat inap");
-//		}
-//	}
-
-//public void periksaPasienRujuk(Pasien Pasien, Penyakit penyakit, String Resep){
-//	if(Pasien.penyakit == penyakit){
-//		System.out.print("Resep obat" + penyakit);
-//	}else{
-//		System.out.print("Surat rujukan ke rumah sakit pusat");
-//	}
-//	}
-//}
-
-//	String Nama;
-//	String Spesialis;
-//	Pasien Pasien;
-//	Penyakit penyakit;
-//	int Status;
-//	String Resep;
-//
-////public void periksaPasien(Pasien Pasien, Penyakit Penyakit, String Resep){
-////	if(Pasien.penyakit == Penyakit){
-////		System.out.print("Resep obat" + Penyakit);
-////	}else{
-////		System.out.print("Surat rujukan ke rumah sakit pusat");
-////	}
-////	}
-//}
-//
-//	String nama;
-//	String spesialis;
-//	Pasien pasien;
-//	Penyakit penyakit;
-//	int status;
-//	String resep;
-//	int hargaDokter;
-//
-//	public void periksaPasien(Pasien Pasien, Penyakit Penyakit, String Resep){
-//		if(Pasien.Penyakit == Penyakit){
-//			System.out.print("Resep obat" + Penyakit);
-//		}else{
-//			System.out.print("Pasien harus rawat inap");
-//		}
-//	}
-
+import java.util.ArrayList; 
+ 
+public class Dokter extends Manusia { 
+  String nama; 
+  String spesialis; 
+  int hargaDokter; 
+ 
+public void periksaPasienRawat(Pasien Pasien, Penyakit Penyakit, String Resep){ 
+    if(Pasien.penyakit){ 
+      System.out.print("Resep obat" + Penyakit); 
+    if(Penyakit.getPenyakitPasien() == "valid"){ 
+      System.out.print("Resep obat" + pasien.getPasienData()); 
+    }else{ 
+      System.out.print("Pasien harus rawat inap"); 
+    } 
+  } 
 }
-
+public void periksaPasienRujuk(Pasien Pasien, Penyakit penyakit, String Resep){ 
+  if(Pasien.penyakit == penyakit){ 
+    System.out.print("Resep obat" + penyakit); 
+  }
+}
+public void periksaPasienRujuk(Pasien Pasien ){ 
+  pasien = Pasien; 
+  ArrayList<Penyakit> dataPasien = pasien.getPasienData(); 
+  for(int i= 0 ; i < dataPasien.size(); i++){ 
+    Penyakit penyakit = dataPasien.get(i); 
+    if(penyakit.penyakitPasien.equals("")){ 
+      System.out.print("Resep obat" + pasien.getPasienData()); 
+    }else { 
+      System.out.print("Surat rujukan ke rumah sakit pusat"); 
+    } 
+  }
+}
+}
